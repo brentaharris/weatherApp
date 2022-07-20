9fb842c44915fa74baaf2f02649e790dd7e7ce7a
brentaharris <brentaustinharris@gmail.com>
2022-07-20 18:55:58 -0400

    updated code

--- src/components/weatherSlice.js
+++ src/components/weatherSlice.js
@@ -3,18 +3,17 @@ import { createSlice } from "@reduxjs/toolkit";
 const initalState = {
   city: [],
   state: [],
-  currTemp: 9,
-  tempHi: 9,
-  tempLow: 9,
+  currTemp: 0,
+  tempHi: 0,
+  tempLow: 0,
   currCondition: [],
-  windSpeed: 9,
+  windSpeed: 0,
   isLoading: true,
 };
 
 const weatherSlice = createSlice({
   name: "weather",
   initalState,
-  reducers: {},
 });
 
 export default weatherSlice.reducer;
